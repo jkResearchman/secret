21679f775fafc9494ae6b820668ae9b3d94325f6
Juned <67226337+jkResearchman@users.noreply.github.com>
2022-05-16 14:18:50 +0530

    secret

---
+++ footer.php
@@ -0,0 +1,35 @@
+<!--footer-->
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 footer-grid-agileits-w3ls text-left">
+					
+				</div>
+				<div class="col-lg-4 footer-grid-agileits-w3ls text-left">
+
+					<div class="tech-btm">
+						<h3>Popular Posts</h3>
+						<?php getpopularposts("page_hits"); ?>
+					</div>
+				</div>
+				<!-- subscribe -->
+				<div class="col-lg-4 subscribe-main footer-grid-agileits-w3ls text-left">
+					<h2>Get Notify Any Community Post </h2>
+					<div class="subscribe-main text-left">
+							<div class="subscribe-form">
+									<form action="#" method="post" class="subscribe_form">
+										<input class="form-control" type="email" placeholder="Enter your email..." required="">
+										<button type="submit" class="btn btn-primary submit">Submit</button>
+									</form>
+									<div class="clearfix"> </div>
+						   </div>
+						<p>We respect your privacy.No spam ever!</p>
+					</div>
+					<!-- //subscribe -->
+				</div>
+			
+				
+			</div>
+			<!-- //footer -->
+		</div>
+	</footer>
